984bfc8925543e1c0a4f619a32258a793d37bb12
Brunof94-debug <brunoprof07@gmail.com>
2025-11-03 01:08:09 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // gera a build em ./dist
+    outDir: 'dist' // <-- gera ./dist na raiz (combina com Output Directory = dist)
   }
 })
