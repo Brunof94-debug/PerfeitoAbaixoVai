32f70f47060b19993790ec1fea272ae2b607e908
Brunof94-debug <brunoprof07@gmail.com>
2025-11-06 20:30:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,14 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  // a root do projeto é a pasta "client"
   root: "client",
   build: {
-    // gerar a pasta dist na raiz do repo (e não dentro de client)
     outDir: "../dist",
-    emptyOutDir: true,
+    emptyOutDir: true
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client/src"),
-    },
-  },
+      "@": path.resolve(__dirname, "client/src")
+    }
+  }
 });
