5145f2d69d7dbd87e3c407a401dbc9468f09543e
Brunof94-debug <brunoprof07@gmail.com>
2025-11-06 20:09:56 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
+// vite.config.ts
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  // a root do projeto é a pasta "client"
+  root: "client",
+  build: {
+    // gerar a pasta dist na raiz do repo (e não dentro de client)
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "client/src"),
+    },
+  },
 });
